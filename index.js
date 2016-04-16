27366e6de14e2692ef67509141861426614f2624
khai96_ <hvksmr1996@gmail.com>
2016-04-16 18:35:06 +0700

    correct parameter names

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 	var tryCatch = (...fnlist) =>
 		_tryCatch(...fnlist.map((fn) => _getfunc(fn, RETURN)));
 
-	var main = module.exports = (act, onsuccess, onerror) => tryCatch(act, onsuccess, onerror);
+	var main = module.exports = (act, onerror, onsuccess) => tryCatch(act, onerror, onsuccess);
 
 	Object.setPrototypeOf(main, {
 		tuple(act) {
